cee8b75be80b807cb8ead6cdc48239c878d60832
Dmitriy Chukhin <dchukhin@gmail.com>
2017-09-23 19:41:39 -0400

    register Category and Transaction models in admin

--- occurrence/admin.py
+++ occurrence/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from . import models
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
+
+@admin.register(models.Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
