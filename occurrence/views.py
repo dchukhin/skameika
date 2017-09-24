752fe1720f37fb00f3ae2ecdc53d80e48dc21ea7
Dmitriy Chukhin <dchukhin@gmail.com>
2017-09-24 17:55:31 -0400

    add pagination by month for transactions view

--- occurrence/views.py
+++ occurrence/views.py
@@ -1,14 +1,31 @@
-from django.shortcuts import render
+from datetime import date
+
+from django.shortcuts import get_object_or_404, render
 
 from . import forms, models
 
 
-def transactions(request):
+def transactions(request, *args, **kwargs):
     """Show all current Transactions."""
-    transactions = models.Transaction.objects.all()
+    # Get the current_month, either from the request kwargs, or use the today's month
+    if request.GET.get('month'):
+        current_month = get_object_or_404(
+            models.Month.objects.all(),
+            name=request.GET.get('month')
+        )
+    else:
+        current_month = models.Month.objects.get_or_create(
+            year=date.today().year,
+            month=date.today().month,
+            name=date.today().strftime('%B, %Y'),
+        )[0]
+    transactions = models.Transaction.objects.filter(month=current_month)
+
     context = {
         'transactions': transactions,
-        'form': forms.TransactionForm()
+        'form': forms.TransactionForm(),
+        'current_month': current_month,
+        'months': models.Month.objects.all(),
     }
     if request.method == 'POST':
         # If this is a POST and the form is valid, then save the form
