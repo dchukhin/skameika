5f8b9f97113e57f16d514ba82a632af54d991e59
Dmitriy Chukhin <dchukhin@gmail.com>
2018-12-12 09:00:07 -0500

    use whitenoise for static file handling

--- skameika/settings.py
+++ skameika/settings.py
@@ -17,6 +17,11 @@ ADMINS = (
 # Application definition
 
 INSTALLED_APPS = [
+    # Disable Django's own staticfiles handling in favour of WhiteNoise, for
+    # greater consistency between gunicorn and `./manage.py runserver`. See:
+    # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
+    'whitenoise.runserver_nostatic',
+
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
