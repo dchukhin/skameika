c55a0f0b710f4f2a2fbacc39a067285f5c8c7508
Dmitriy Chukhin <6025153+dchukhin@users.noreply.github.com>
2025-09-15 15:21:11 -0400

    create a CSVImport model for CSV imports

--- data_tools/models.py
+++ data_tools/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class CSVImport(models.Model):
+    """
+    Model to track CSV imports.
+    """
+
+    file = models.FileField(upload_to="csv_imports/")
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"CSV Import on {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
